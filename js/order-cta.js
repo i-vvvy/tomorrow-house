d5ec27225b24efead3ed1787419af797f53d6a07
i_vvvy <ivvvy.e@gmail.com>
2022-04-02 18:06:58 +0900

    🚸  Toggle aria-label text of bookmark-button

--- js/order-cta.js
+++ js/order-cta.js
@@ -25,18 +25,17 @@ function toggleOrderCtaBookmark() {
 
   let newCount = count
   if (this.classList.contains('is-active')) {
-    // NOTE: 활성화 된 상태이니 => 비활성화 (ic-bookmark)
     icon.classList.add('ic-bookmark')
     icon.classList.remove('ic-bookmark-filled')
     newCount = newCount - 1
   } else {
-    // NOTE: 비활성화 된 상태이니 => 활성화 (ic-bookmark-filled)
     icon.classList.add('ic-bookmark-filled')
     icon.classList.remove('ic-bookmark')
     newCount = newCount + 1
   }
 
   countSpan.innerHTML = newCount.toLocaleString()
+  countSpan.setAttribute('aria-label', `북마크 ${newCount.toLocaleString()}회`)
   this.classList.toggle('is-active')
 }
 
