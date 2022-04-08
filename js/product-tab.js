42ddff66e30fe0dd5a82f35dd65b8847408fd82a
i_vvvy <ivvvy.e@gmail.com>
2022-04-08 14:05:02 +0900

    🐛  Fix bugs on scroll event handler

--- js/product-tab.js
+++ js/product-tab.js
@@ -5,14 +5,20 @@ const TOP_HEADER_DESKTOP = 80 + 50 + 54
 const TOP_HEADER_MOBILE = 50 + 40 + 40
 
 let currentActiveTab = productTab.querySelector('.is-active')
+let disableUpdating = false
 
 function toggleActiveTab() {
   const tabItem = this.parentNode
 
   if (currentActiveTab !== tabItem) {
+    disableUpdating = true
     tabItem.classList.add('is-active')
     currentActiveTab.classList.remove('is-active')
     currentActiveTab = tabItem
+
+    setTimeout(() => {
+      disableUpdating = false
+    }, 1000)
   }
 }
 
@@ -55,9 +61,14 @@ function detectTabPanelPosition() {
     const position = window.scrollY + panel.getBoundingClientRect().top
     productTabPanelPositionMap[id] = position
   })
+
+  updateActiveTabOnScroll()
 }
 
 function updateActiveTabOnScroll() {
+  if (disableUpdating) {
+    return
+  }
   const scrolledAmount =
     window.scrollY +
     (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP + 80 : TOP_HEADER_MOBILE + 8)
@@ -75,12 +86,25 @@ function updateActiveTabOnScroll() {
     newActiveTab = productTabButtonList[0] // 상품정보 버튼
   }
 
+  // 추가: 끝까지 스크롤을 한 경우 newActiveTab = productTabButtonList[4]
+  // window.scrollY + window.innerHeight === body의 전체 height
+  // window.innerWidth < 1200 - orderCta, 56px
+
+  const bodyHeight =
+    document.body.offsetHeight + (window.innerWidth < 1200 ? 56 : 0)
+  if (window.scrollY + window.innerHeight === document.body.offsetHeight) {
+    newActiveTab = productTabButtonList[4]
+  }
+
   if (newActiveTab) {
     newActiveTab = newActiveTab.parentNode
 
     if (newActiveTab !== currentActiveTab) {
       newActiveTab.classList.add('is-active')
-      currentActiveTab.classList.remove('is-active')
+      if (currentActiveTab !== null) {
+        currentActiveTab.classList.remove('is-active')
+      }
+
       currentActiveTab = newActiveTab
     }
   }
